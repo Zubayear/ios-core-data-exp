928c2a7f05820b11d7f89f8c72d10b67df5f2a88
jubaer <syedjubaer997@gmail.com>
2020-08-11 00:55:19 -0700

    goals view controller

--- ios-core-data-exp/GoalVC.swift
+++ ios-core-data-exp/GoalVC.swift
@@ -2,11 +2,15 @@ import UIKit
 
 class GoalVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+        print("Debug: Goal Btn pressed")
+    }
+    
 }
 
