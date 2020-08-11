424072dc0ee966dc9f6e60810a6a9c39b6f61eb5
jubaer <syedjubaer997@gmail.com>
2020-08-11 08:29:12 -0700

    create goal  view controller

---
+++ ios-core-data-exp/View/CreateGoalVC.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+    
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+        
+    }
+    
+}
